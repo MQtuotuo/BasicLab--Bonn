274dc346c47034a319b12b7352f1de986182149b
Ming Qu <ming.qu0202@gmail.com>
2018-07-16 17:29:13 +0200

    procedure model

---
+++ modelling/modelling/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  modelling
+//
+//  Created by QuMing on 16/7/2018.
+//  Copyright © 2018 QuMing. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
